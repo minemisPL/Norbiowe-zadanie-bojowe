7d76630aa9c09b76fa09e04135950b2ecb35270d
Rollczi <ndejlich5@gmail.com>
2021-04-19 14:29:26 +0200

    bug fix

--- src/main/java/me/minemis/norbiowezadaniebojowe/listeners/PlayerJoin.java
+++ src/main/java/me/minemis/norbiowezadaniebojowe/listeners/PlayerJoin.java
@@ -1,19 +1,23 @@
 package me.minemis.norbiowezadaniebojowe.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PlayerJoin implements Listener {
 
-    private static long logTime;
+    private static Map<String, Long> logTime = new HashMap<>();
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
-        logTime = System.currentTimeMillis();
+        logTime.put(event.getPlayer().getName(), System.currentTimeMillis());
     }
 
-    public static long getLogTime(){
-        return logTime;
+    public static long getLogTime(Player player) {
+        return logTime.get(player.getName());
     }
 }
